7098e125daeba0865fde3de432953d5af427f2df
Andrew Turner <36438592+roomofwires@users.noreply.github.com>
2020-01-09 10:31:50 +0000

    Made jumbotron image random and improved animation

--- js/jumbotron.js
+++ js/jumbotron.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
-  $('.jumbotron').css({'background-image': 'linear-gradient(rgba(0,0,0,0.25), rgba(0,0,0,0.25)), url(img/jumbo.jpg)'});
-  $('.jumbotron').mousemove(function(e) {
-    var amountMovedX = (e.pageX * -1/60);
-    var amountMovedY = (e.pageY * -1/60);
-    $(this).css('background-position', amountMovedX + 'px ' + amountMovedY + 'px');
+  // Randomise jumbotron background image
+  var images = ['jumbo01.jpg', 'jumbo02.jpg', 'jumbo03.jpg'];
+  $('.jumbotron').css({'background-image': 'linear-gradient(rgba(0,0,0,0.25), rgba(0,0,0,0.25)), url(img/'
+    + images[Math.floor(Math.random() * images.length)] + ')'});
+
+  // Zoom jumbotron background image on mouseover
+  $('.jumbotron').mouseenter(function(e) {
+    $(this).animate({'background-size': '120%'}, 500, function() {
+      $(this).animate({'background-size': '100%'}, 500);
+    });
   });
 });
