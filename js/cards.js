e373d31e9c578e1f78c95222efdc75efec10f536
Andrew Turner <36438592+roomofwires@users.noreply.github.com>
2020-01-10 13:14:35 +0000

    Improvements to card animation

--- js/cards.js
+++ js/cards.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
   // Animate hovering over cards
-  $('.col-lg-3 .card-img-top').mouseenter(function(e) {
-    $(this).animate({'width': '105%', 'height': 'auto'}, 250, function() {
-    $(this).animate({'width': '100%', 'height': 'auto'}, 500);
+  $('.card-img-top').mouseenter(function(e) {
+    var shift = -1 * (($(this)[0].width * 1.05) - $(this)[0].width) / 2;
+
+    $(this).animate({'width': '105%', 'height': 'auto', 'margin': '0px ' + shift + 'px'}, 250, function() {
+      $(this).animate({'width': '100%', 'height': 'auto', 'margin': '0 0'}, 500);
     });
   });
 });
