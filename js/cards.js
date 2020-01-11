071bac520cb6d12e7e495e67ba924e53b2df0b87
Andrew Turner <36438592+roomofwires@users.noreply.github.com>
2020-01-11 10:40:34 +0000

    Changed card animation

--- js/cards.js
+++ js/cards.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
   // Animate hovering over cards
   $('.card-img-top').mouseenter(function(e) {
-    var shift = -1 * (($(this)[0].width * 1.05) - $(this)[0].width) / 2;
-
-    $(this).animate({'width': '105%', 'height': 'auto', 'margin': '0px ' + shift + 'px'}, 250, function() {
-      $(this).animate({'width': '100%', 'height': 'auto', 'margin': '0 0'}, 500);
+    $(this).animate({'opacity': 0.5}, 250, function() {
+      $(this).animate({'opacity': 1.0}, 500);
     });
   });
 });
