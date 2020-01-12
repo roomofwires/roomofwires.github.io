b8df6e5ebca15a277b6ff22fada9592f9d86edf0
Andrew Turner <36438592+roomofwires@users.noreply.github.com>
2020-01-12 10:18:56 +0000

    Fixed animation on about page

--- js/cards.js
+++ js/cards.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   // Animate hovering over cards
-  $('.card-img-top').mouseenter(function(e) {
+  $('.col-lg-3 .card-img-top').mouseenter(function(e) {
     $(this).animate({'opacity': 0.5}, 250, function() {
       $(this).animate({'opacity': 1.0}, 500);
     });
