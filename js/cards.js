ae22d5f7579aa7294642f9dd066654a6982bd0a3
Andrew Turner <36438592+roomofwires@users.noreply.github.com>
2020-01-12 11:33:02 +0000

    Changed card animation behaviour

--- js/cards.js
+++ js/cards.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
   // Animate hovering over cards
   $('.col-lg-3 .card-img-top').mouseenter(function(e) {
-    $(this).animate({'opacity': 0.5}, 250, function() {
-      $(this).animate({'opacity': 1.0}, 500);
-    });
+    $(this).animate({'opacity': 0.5}, 250);
+  });
+  $('.col-lg-3 .card-img-top').mouseleave(function(e) {
+    $(this).animate({'opacity': 1.0}, 250);
   });
 });
