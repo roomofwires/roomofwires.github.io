c89ee49c8d312f648b4bf024cb19a3417ce4b150
Andrew Turner <36438592+roomofwires@users.noreply.github.com>
2021-01-20 13:57:24 +0000

    Tweaked landing page so text doesn't glitch

--- js/glitch.js
+++ js/glitch.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
   // Make background image glitchy
   $('.glitch-img').mgGlitch({
-    destroy : false,
+    destroy: false,
     glitch: true, // set 'false' to stop glitching
     scale: true, // set 'false' to stop scaling
-    blend : true, // set 'false' to stop glitch blending
-    blendModeType : 'hue', // select blend mode type
-    glitch1TimeMin : 600, // set min time for glitch 1 elem
-    glitch1TimeMax : 900, // set max time for glitch 1 elem
-    glitch2TimeMin : 10, // set min time for glitch 2 elem
-    glitch2TimeMax : 115, // set max time for glitch 2 elem
-    zIndexStart : -1, // because of absolute position, set z-index base value
+    blend: true, // set 'false' to stop glitch blending
+    blendModeType: 'hue', // select blend mode type
+    glitch1TimeMin: 600, // set min time for glitch 1 elem
+    glitch1TimeMax: 900, // set max time for glitch 1 elem
+    glitch2TimeMin: 10, // set min time for glitch 2 elem
+    glitch2TimeMax: 115, // set max time for glitch 2 elem
+    zIndexStart: -10, // because of absolute position, set z-index base value
   });
 });
